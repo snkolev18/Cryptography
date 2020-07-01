935d2e748839947704fec41867c038b4782a3d78
snkolev18 <SNKolev@codingburgas.bg>
2020-07-01 14:10:40 +0300

    encrypt and decrypt the string using the key

--- cryptographer.py
+++ cryptographer.py
@@ -1,5 +1,16 @@
 from cryptography.fernet import Fernet #used for the encryption of the data
 
 if __name__ == "__main__":
-    pub_key = Fernet.generate_key()
-    print(pub_key)
+    priv_key = Fernet.generate_key()
+    print(priv_key)
+
+    message = "Imam 200k leva vkushti i parolata za seifa e 1234"
+    msg_bin = message.encode()
+
+    enc = Fernet(priv_key)
+
+    enc_msg = enc.encrypt(msg_bin) #encrypting the message
+    print(enc_msg)
+
+    dec_msg = enc.decrypt(enc_msg) #decrypting the message
+    print(dec_msg)
